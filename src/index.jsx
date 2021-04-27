d9036b5d469b2d6868b6a9e25fe22c8157f8c3a8
wqgc <wqgc@users.noreply.github.com>
2021-04-27 12:52:16 -0700

    testing pushing an error

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));;;;
+
+this should cause an error!!
 
 // Just a comment to test branch-switching!
 // Let's try this again
